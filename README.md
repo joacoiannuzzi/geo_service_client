e21e33fd50a6bd8175e248e197da4d2498456b65
Joaquin Iannuzzi <joaco.iannuzzi@gmail.com>
2021-04-08 13:21:54 -0300

    waits to finish; fixed cli and other changes

---
+++ README.md
@@ -0,0 +1,9 @@
+Listens in ports 50.003, 50.004 and 50.005
+
+To run
+
+    sbt "run --file ./examples/ips.txt"  
+
+or 
+
+    sbt "run 8.8.8.8,53.63.34.3,24.4.2.6"
--- src/main/scala/client/Client.scala
+++ src/main/scala/client/Client.scala
@@ -1,34 +1,22 @@
 package client
 
+import client.Util.createStub
 import io.grpc.ManagedChannelBuilder
-import service.geoService.GeoServiceGrpc.{GeoServiceStub, stub}
-import service.geoService.{
-  GeoServiceGrpc,
-  GetLocationByIpReply,
-  GetLocationByIpRequest,
-  HealthCheckReq
-}
+import org.rogach.scallop._
+import service.geoService.GeoServiceGrpc.GeoServiceStub
+import service.geoService._
 
 import scala.annotation.tailrec
 import scala.collection.mutable
-import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-import scala.io.{Source, StdIn}
+import scala.concurrent.{Await, Future}
+import scala.io.Source
 import scala.language.postfixOps
 import scala.util.{Failure, Success, Try}
 
-object Client extends App {
-
-  import org.rogach.scallop._
-
-  class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
-    val file = opt[String]()
-    val ips = trailArg[String]()
-    verify()
-  }
-
-  def createStub(port: Int) = {
+package object Util {
+  def createStub(port: Int): GeoServiceStub = {
     val builder =
       ManagedChannelBuilder.forAddress("localhost", port)
 
@@ -37,70 +25,77 @@ object Client extends App {
 
     GeoServiceGrpc.stub(channel)
   }
+}
 
-  val q = 2
+object Client extends App {
+
+  class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
+    val file = opt[String]()
+    val ips = trailArg[String](required = false)
+    verify()
+  }
+  val conf = new Conf(args)
+
+  val ipList = conf.file
+    .map { path =>
+      val source = Source.fromFile(path)
+      val ips = source.getLines().toList
+      source.close()
+      ips
+    }
+    .getOrElse {
+      if (!conf.ips.isSupplied) {
+        System.err.println("ERROR :: No ips supplied")
+        sys.exit(1)
+      }
+      conf.ips.map(_.split(',').toList).getOrElse(List())
+    }
+
+  val q = 3
   val initialPort = 50_003
 
   val stubs = (initialPort until initialPort + q).map(createStub).toList
 
-//  stubs.head.getLocationByIp(GetLocationByIpRequest("8.8.8.8")).onComplete {
-//    case Success(GetLocationByIpReply(country, state, _)) =>
-//      println(s"ip: 8.8.8.8 -> $country, $state")
-//    case Failure(exception) =>
-//      System.err.println("Failed ip 8.8.8.8")
-//  }
-
-  StdIn.readLine()
-
-//  val conf = new Conf(args)
-
-//  val ipList = conf.file
-//    .map { path =>
-//      val source = Source.fromFile(path)
-//      val ips = source.getLines()
-//      source.close()
-//      ips.toList
-//    }
-//    .getOrElse {
-//      if (!conf.ips.isSupplied) throw new RuntimeException("No ips supplied")
-//      conf.ips.map(_.split(',').toList).getOrElse(List())
-//    }
-
-  val ipList = List(
-    "8.8.8.8",
-    "88.8.8.8",
-    "8.86.8.8",
-    "8.86.8.77",
-    "8.86.83.8",
-    "8.86.83.8",
-    "8.8.83.8",
-    "8.86.84.8",
-    "8.86.43.8",
-    "8.8.8.8",
-    "8.86.48.8"
-  )
-
-  val balancer = new Balancer(stubs)
+  val balancer = Balancer(stubs)
 
   ipList.foreach { ip =>
     balancer.run(_.getLocationByIp(GetLocationByIpRequest(ip))) {
-      case Success(GetLocationByIpReply(country, state, _)) =>
-        println(s"ip: $ip -> country: $country and state: $state")
+      case Success(GetLocationByIpReply(country, _, _)) => {
+        balancer.run(_.getStatesOfCountry(GetStatesOfCountryRequest(country))) {
+          case Success(GetStatesOfCountryReply(states, _)) =>
+            println(
+              s"ip: $ip -> \n\tcountry: $country \n\tand states: ${states
+                .mkString("\n\t\t", "\n\t\t", "")}"
+            )
+          case Failure(_) =>
+            System.err.println(
+              s"Failed to get states of country $country from ip $ip"
+            )
+        }
+      }
       case _ => System.err.println(s"Failed to get ip: $ip")
     }
   }
 
-  StdIn.readLine()
+  balancer.await()
 }
 
-class Balancer(stubs: List[GeoServiceStub]) {
+case class Balancer(stubs: List[GeoServiceStub]) {
 
   private val healthyStubs = mutable.Set[Int]()
   private val workingStubs = mutable.Set[Int]()
+  checkHealth()
 
-  check()
+  private var lastCheck = System.currentTimeMillis()
+  private var timeSinceServersDown = 0L
+  private var calls = 0
+  private var responses = 0
 
-  private def check(): Unit = {
+  def await(): Unit = {
+    while (calls != responses) print("") // without the print it doesn't work
+  }
+
+  private def checkHealth(): Unit = {
     stubs.zipWithIndex.foreach { case (s, i) =>
       val future = s.healthCheck(HealthCheckReq())
       future.onComplete {
@@ -111,32 +106,67 @@ class Balancer(stubs: List[GeoServiceStub]) {
     }
   }
 
-  @tailrec
-  final def run[T](
-      f: GeoServiceStub => Future[T]
-  )(res: Try[T] => Unit): Unit = {
+  def run[T](
+      caller: GeoServiceStub => Future[T]
+  )(responder: Try[T] => Unit): Unit = {
+    calls = calls + 1
 
-    if (healthyStubs.isEmpty) {
-      System.err.println("ERROR :: All servers are down")
-      System.exit(1)
+    def runInside(
+        caller: GeoServiceStub => Future[T]
+    )(responder: Try[T] => Unit)(n: Int): Unit = {
+
+      runAux(caller) {
+        case Failure(e) =>
+          if (n > 5) {
+            responses = responses + 1
+            responder(Failure(e))
+          } else runInside(caller)(responder)(n + 1)
+
+        case Success(value) =>
+          responses = responses + 1
+          responder(Success(value))
+      }
+    }
+    runInside(caller)(responder)(0)
+  }
+
+  @tailrec
+  private final def runAux[T](
+      caller: GeoServiceStub => Future[T]
+  )(responder: Try[T] => Unit): Unit = {
+
+    if (System.currentTimeMillis() - lastCheck > 500) {
+      checkHealth()
+      lastCheck = System.currentTimeMillis()
     }
 
-    val available = stubs.zipWithIndex
-      .find { case (_, i) =>
-        healthyStubs.contains(i) && !workingStubs.contains(i)
+    if (healthyStubs.isEmpty) {
+      if (System.currentTimeMillis() - timeSinceServersDown > 2000) {
+        System.err.println("ERROR :: All servers are down")
+        System.exit(1)
       }
-
-    if (available.isEmpty) {
-      check()
-      run(f)(res)
+      if (timeSinceServersDown == 0) {
+        timeSinceServersDown = System.currentTimeMillis()
+      }
+      runAux(caller)(responder)
 
     } else {
-      val (stub, index) = available.get
-      workingStubs add index
-      val future = f(stub)
-      future.onComplete(res)
-      future.onComplete { _ =>
-        workingStubs remove index
+
+      timeSinceServersDown = 0
+
+      val available = stubs.zipWithIndex
+        .find { case (_, i) =>
+          healthyStubs.contains(i) && !workingStubs.contains(i)
+        }
+
+      available match {
+        case Some((stub, index)) => {
+          workingStubs add index
+          val future = caller(stub)
+          future.onComplete(_ => workingStubs remove index)
+          future.onComplete(responder)
+        }
+        case None => runAux(caller)(responder)
       }
     }
   }
