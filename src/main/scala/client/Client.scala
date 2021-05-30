a2d5ee719cbe552b851fd8304bae8d77dac68f2a
Elias <eliasleonov99@gmail.com>
2021-05-29 21:17:26 -0300

    fixes and more fixes

--- src/main/scala/client/Client.scala
+++ src/main/scala/client/Client.scala
@@ -3,6 +3,8 @@ package client
 import client.BalancerUtils.getStubs
 import client.Util.createStub
 import client.Utils.getIpList
+import com.google.common.base.Utf8
+import io.etcd.jetcd.kv.GetResponse
 import io.etcd.jetcd.options.{GetOption, WatchOption}
 import io.etcd.jetcd.watch.{WatchEvent, WatchResponse}
 import io.etcd.jetcd.{ByteSequence, Client, KV, Watch}
@@ -11,7 +13,7 @@ import service.geoService.GeoServiceGrpc.{GeoServiceStub, stub}
 import service.geoService._
 
 import java.nio.charset.{Charset, StandardCharsets}
-import java.util.concurrent.CountDownLatch
+import java.util.concurrent.{CompletableFuture, CountDownLatch}
 import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
@@ -72,7 +74,6 @@ object Client2 extends App {
       case _ => System.err.println(s"Failed to get ip: $ip")
     }
   }
-
   StdIn.readLine()
 }
 
@@ -80,67 +81,49 @@ case class Balancer() {
 
   val client: Client =
     Client.builder().endpoints("http://localhost:2379").build()
+  val key: ByteSequence = ByteSequence.from("service/geo".getBytes())
 
   val kvClient: KV = client.getKVClient
   val watchClient: Watch = client.getWatchClient
 
   val stubs = getStubs(kvClient)
 
-  private val healthyStubs = mutable.Set[Int]()
-  private val workingStubs = mutable.Set[Int]()
+//  private val healthyStubs = mutable.Set[Int]()
+  private val workingStubs = mutable.Set[String]()
 
-  var stubsMutable: ListBuffer[GeoServiceStub] = ListBuffer()
-  stubsMutable ++= stubs
 
   checkStubs()
 
   private def checkStubs(): Unit = {
     val key = ByteSequence.from("service/geo/", StandardCharsets.UTF_8)
-    val latch: CountDownLatch = new CountDownLatch(1)
 
     val listener = Watch.listener((response: WatchResponse) => {
-      print("Watching for key=something")
+      print(s"Watching for key = $response")
 
       response.getEvents
         .stream()
-        .map(event => {
+        .forEach(event => {
+          println("ASDFASDFASD")
+          println(event.getEventType)
+
           event.getEventType match {
             case WatchEvent.EventType.PUT =>
-              stubsMutable += createStub(
-                event.getKeyValue.getValue.toString(Charset.forName("UTF-8"))
-              )
-            case WatchEvent.EventType.DELETE =>
-              stubsMutable -= createStub(
-                event.getKeyValue.getValue.toString(Charset.forName("UTF-8"))
-              )
+              stubs.put(event.getKeyValue.getKey.toString(Charset.forName("UTF-8")), createStub(event.getKeyValue.getValue.toString(Charset.forName("UTF-8"))))
+            case WatchEvent.EventType.DELETE =>{
+              stubs.remove(event.getKeyValue.getKey.toString(Charset.forName("UTF-8")))
+              workingStubs.remove(event.getKeyValue.getKey.toString(Charset.forName("UTF-8")))
+            }
             case WatchEvent.EventType.UNRECOGNIZED => None
           }
         })
     })
 
-    try {
-      val watchOption = WatchOption.newBuilder().withPrefix(key).build()
-      val watch: Watch = watchClient
-      val watcher: Watch.Watcher = watch.watch(key, watchOption, listener)
-      try {
-        println("before latch")
-        latch.await()
-        println("after latch")
+    println("before watch")
+    val watchOption = WatchOption.newBuilder().withPrefix(key).build()
+    val watch: Watch = watchClient
+    val watcher: Watch.Watcher = watch.watch(key, watchOption, listener)
+    println("after watch")
 
-      } catch {
-        case e: Exception =>
-          System.exit(1)
-      } finally {
-        if (client != null) client.close()
-        if (watch != null) watch.close()
-        if (watcher != null) watcher.close()
-        if (kvClient != null) kvClient.close()
-        if (watchClient != null) watchClient.close()
-      }
-    } catch {
-      case e: Exception =>
-        System.exit(1)
-    }
   }
 
   def run[T](
@@ -170,16 +153,16 @@ case class Balancer() {
       caller: GeoServiceStub => Future[T]
   )(responder: Try[T] => Unit): Unit = {
 
-    val available = stubs.zipWithIndex
-      .find { case (_, i) =>
-        healthyStubs.contains(i) && !workingStubs.contains(i)
+    val available = stubs
+      .find { case (k, _) =>
+        !workingStubs.contains(k)
       }
 
     available match {
-      case Some((stub, index)) =>
-        workingStubs add index
+      case Some((key, stub)) =>
+        workingStubs add key
         val future = caller(stub)
-        future.onComplete(_ => workingStubs remove index)
+        future.onComplete(_ => workingStubs remove key)
         future.onComplete(responder)
       case None => runAux(caller)(responder)
     }
@@ -187,31 +170,40 @@ case class Balancer() {
 }
 
 object BalancerUtils {
+  val key: ByteSequence = ByteSequence.from("service/geo".getBytes())
 
   def getStubs(kvClient: KV) = {
-
-    val key: ByteSequence = ByteSequence.from("service/geo".getBytes())
-
-    val keyValueMap = mutable.Map[String, String]()
-
+    val keyValueMap = mutable.Map[String,String]()
     try {
-      val option = GetOption.newBuilder.withRange(key).build
-      val futureResponse = kvClient.get(key, option)
-      val response = futureResponse.get
-      response.getKvs
-        .stream()
-        .forEach(kv => {
-          keyValueMap.put(
-            kv.getKey.toString(Charset.forName("UTF-8")),
-            kv.getValue.toString(Charset.forName("UTF-8"))
-          )
-        })
+
+      val option: GetOption = GetOption.newBuilder()
+        .withSortField(GetOption.SortTarget.KEY)
+        .withSortOrder(GetOption.SortOrder.DESCEND)
+        .withPrefix(key)
+        .build()
+
+      val futureResponse = kvClient.get(key, option);
+
+      val response: GetResponse = futureResponse.get();
+
+      if (response.getKvs().isEmpty()) {
+        print("Failed to retrieve any key.");
+      }
+
+      response.getKvs.forEach(x => {
+        keyValueMap.put(x.getKey.toString(Charset.forName("UTF-8")), x.getValue.toString(Charset.forName("UTF-8")))
+      })
+
+      println("Retrieved " + response.getKvs().size() + " keys.");
+
+
     } catch {
-      case e: Exception =>
-        throw new RuntimeException("Failed to retrieve any key.", e)
+      case e: Exception => print("Failed to retrieve any key.");
     }
-    //  val stubs = keyValueMap.values.map(createStub).toList
-    val stubs = keyValueMap.values.map(x => createStub(x)).toList
-    stubs
+    println(keyValueMap)
+    keyValueMap.map {
+      case (key, value) => (key, createStub(value))
+    }
+
   }
 }
